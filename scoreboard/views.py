fe886b719813c1db0cd333ad3bfe588716e49d64
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2023-01-26 21:40:02 +0200

    Add files via upload

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from accounts.models import Profile as User
 # Create your views here.
 def index(request):
-    return render(request, 'scoreboard.html')
+    users = User.objects.all().order_by('-points')
+    for user in users:
+        print(user.username, user.points)
+    return render(request, 'scoreboard.html', {'users':users})
--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,42 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
+const homeLinkElement = document.getElementById('home-link');
+const jobsLinkElement= document.getElementById('projects-link');
+const projectsCreateLink= document.getElementById('projects-create-link');
+const scoreboardLink= document.getElementById('scoreboard-link');
+const teamsLink= document.getElementById('teams-link');
+const profileLink= document.getElementById('profile-link');
+const loginLink= document.getElementById('login-link');
+const registerLink= document.getElementById('register-link');
+const myJobLink = document.getElementById('my-job-link');
+console.log(document.URL);
+if(document.URL==='http://127.0.0.1:8000/projects/'){
+	jobsLinkElement.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/projects/create_job_page/'){
+	projectsCreateLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/scoreboard/'){
+	scoreboardLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/teams/'){
+	teamsLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/profile/'){
+	profileLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/accounts/'){
+	loginLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/accounts/registration_page'){
+	registerLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
+}
+if(document.URL==='http://127.0.0.1:8000/projects/jobs_created_by_user/'){
+	myJobLink.classList.add('active');
+	homeLinkElement.classList.remove('active');
 }
