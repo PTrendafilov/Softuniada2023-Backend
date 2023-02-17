7f2eeef9f33b0f1d33487add2814fca6d6c28bcc
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2023-02-17 11:12:34 +0200

    Add files via upload

--- profile_details/views.py
+++ profile_details/views.py
@@ -3,6 +3,5 @@ from .models import CV
 from accounts.models import Profile as User
 # Create your views here.
 def index(request):
-    
     content = {}
     return render(request, 'profile-details-page.html')
